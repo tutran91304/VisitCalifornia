f31e96ef2ca76acc93fd85ce258c1dbab425855b
Tu Tran <tu.tran2@yahoo.com>
2020-04-06 22:04:27 -0700

    Dependencies and GeometryExtensions compiling

---
+++ VisitCalifornia/Helpers/GeometryExtensions.cs
@@ -0,0 +1,85 @@
+﻿using NetTopologySuite;
+using NetTopologySuite.Geometries;
+using ProjNet;
+using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
+using System.Collections.Generic;
+
+namespace Tourism.Utils
+{
+    static class GeometryExtensions
+    {
+        static readonly NtsGeometryServices _geometryServices = NtsGeometryServices.Instance;
+        static readonly CoordinateSystemServices _coordinateSystemServices
+            = new CoordinateSystemServices(
+                new CoordinateSystemFactory(),
+                new CoordinateTransformationFactory(),
+                new Dictionary<int, string>
+                {
+                // Coordinate systems:
+
+                // (3857 and 4326 included automatically)
+
+                // This coordinate system covers the area of our data.
+                // Different data requires a different coordinate system.
+                [2855] =
+                    @"
+                    PROJCS[""NAD83(HARN) / Washington North"",
+                        GEOGCS[""NAD83(HARN)"",
+                            DATUM[""NAD83_High_Accuracy_Regional_Network"",
+                                SPHEROID[""GRS 1980"",6378137,298.257222101,
+                                    AUTHORITY[""EPSG"",""7019""]],
+                                AUTHORITY[""EPSG"",""6152""]],
+                            PRIMEM[""Greenwich"",0,
+                                AUTHORITY[""EPSG"",""8901""]],
+                            UNIT[""degree"",0.01745329251994328,
+                                AUTHORITY[""EPSG"",""9122""]],
+                            AUTHORITY[""EPSG"",""4152""]],
+                        PROJECTION[""Lambert_Conformal_Conic_2SP""],
+                        PARAMETER[""standard_parallel_1"",48.73333333333333],
+                        PARAMETER[""standard_parallel_2"",47.5],
+                        PARAMETER[""latitude_of_origin"",47],
+                        PARAMETER[""central_meridian"",-120.8333333333333],
+                        PARAMETER[""false_easting"",500000],
+                        PARAMETER[""false_northing"",0],
+                        UNIT[""metre"",1,
+                            AUTHORITY[""EPSG"",""9001""]],
+                        AUTHORITY[""EPSG"",""2855""]]
+                "
+                });
+
+        public static Geometry ProjectTo(this Geometry geometry, int srid)
+        {
+            var geometryFactory = _geometryServices.CreateGeometryFactory(srid);
+            var transformation = _coordinateSystemServices.CreateTransformation(geometry.SRID, srid);
+
+            return Transform(geometry, transformation.MathTransform);
+        }
+
+        public static Geometry Transform(this Geometry geometry, MathTransform mathTransform)
+        {
+            geometry = geometry.Copy();
+            geometry.Apply(new MathTransformFilter(mathTransform));
+            return geometry;
+        }
+
+        private sealed class MathTransformFilter : ICoordinateSequenceFilter
+        {
+            private readonly MathTransform _mathTransform;
+
+            public MathTransformFilter(MathTransform mathTransform)
+                => _mathTransform = mathTransform;
+
+            public bool Done => false;
+            public bool GeometryChanged => true;
+
+            public void Filter(CoordinateSequence seq, int i)
+            {
+                var (x, y, z) = _mathTransform.Transform(seq.GetX(i), seq.GetY(i), seq.GetZ(i));
+                seq.SetX(i, x);
+                seq.SetY(i, y);
+                seq.SetZ(i, z);
+            }
+        }
+    }
+}
---
+++ VisitCalifornia/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VisitCalifornia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
