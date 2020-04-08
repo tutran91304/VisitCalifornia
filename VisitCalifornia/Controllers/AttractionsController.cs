8a88d33b142f78c12ad007569482fac579171059
Tu Tran <tu.tran2@yahoo.com>
2020-04-07 18:06:33 -0700

    Working with GeometryExtensions

--- VisitCalifornia/Controllers/AttractionsController.cs
+++ VisitCalifornia/Controllers/AttractionsController.cs
@@ -36,64 +36,7 @@ namespace VisitCalifornia.Controllers
             var location = new Point(indexModel.SearchInput.Latitude, indexModel.SearchInput.Longitude) { SRID = 4326 };
 
 
-            /*TESTING*/
-            var testlocation = new Point(34.4272181, -118.5995318) { SRID = 4326 };
-            var originWKT = "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.01745329251994328,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4326\"]]";
-            //var targetWKT = "PROJCS[\"US National Atlas Equal Area\",GEOGCS[\"Unspecified datum based upon the Clarke 1866 Authalic Sphere\",DATUM[\"Not_specified_based_on_Clarke_1866_Authalic_Sphere\",SPHEROID[\"Clarke 1866 Authalic Sphere\",6370997,0,AUTHORITY[\"EPSG\",\"7052\"]],AUTHORITY[\"EPSG\",\"6052\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4052\"]],PROJECTION[\"Lambert_Azimuthal_Equal_Area\"],PARAMETER[\"latitude_of_center\",45],PARAMETER[\"longitude_of_center\",-100],PARAMETER[\"false_easting\",0],PARAMETER[\"false_northing\",0],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],AXIS[\"X\",EAST],AXIS[\"Y\",NORTH],AUTHORITY[\"EPSG\",\"2163\"]]";
-            var targetWKT = @"PROJCS[""NAD83 / UTM zone 11N"",
-                        GEOGCS[""NAD83"",
-                            DATUM[""North_American_Datum_1983"",
-                                SPHEROID[""GRS 1980"",6378137,298.257222101,
-                                    AUTHORITY[""EPSG"",""7019""]],
-                                TOWGS84[0,0,0,0,0,0,0],
-                                AUTHORITY[""EPSG"",""6269""]],
-                            PRIMEM[""Greenwich"",0,
-                                AUTHORITY[""EPSG"",""8901""]],
-                            UNIT[""degree"",0.0174532925199433,
-                                AUTHORITY[""EPSG"",""9122""]],
-                            AUTHORITY[""EPSG"",""4269""]],
-                        PROJECTION[""Transverse_Mercator""],
-                        PARAMETER[""latitude_of_origin"",0],
-                        PARAMETER[""central_meridian"",-117],
-                        PARAMETER[""scale_factor"",0.9996],
-                        PARAMETER[""false_easting"",500000],
-                        PARAMETER[""false_northing"",0],
-                        UNIT[""metre"",1,
-                            AUTHORITY[""EPSG"",""9001""]],
-                        AXIS[""Easting"",EAST],
-                        AXIS[""Northing"",NORTH],
-                        AUTHORITY[""EPSG"",""26911""]]";
-
-
-
-            var coordinateSystemFactory = new ProjNet.CoordinateSystems.CoordinateSystemFactory();
-            var transformationFactory = new ProjNet.CoordinateSystems.Transformations.CoordinateTransformationFactory();
-
-            var originCoordinateSystem = coordinateSystemFactory.CreateFromWkt(originWKT);
-            var targetCoordinateSystem = coordinateSystemFactory.CreateFromWkt(targetWKT);
-
-            var transform = transformationFactory.CreateFromCoordinateSystems(originCoordinateSystem, targetCoordinateSystem);
-
-
-            
-            double[] loc = new double[2];
-            loc[0] = location.Y;
-            loc[1] = location.X;
-
-
-            double[] loc2 = new double[2];
-            loc2[0] = testlocation.Y;
-            loc2[1] = testlocation.X;
-
-            var newPointA = transform.MathTransform.Transform(loc);
-            var newPointB = transform.MathTransform.Transform(loc2);
-
-            Point pointA = new Point(newPointA[1], newPointA[0]) { SRID = 2163 };
-            Point pointB = new Point(newPointB[1], newPointB[0]) { SRID = 2163 };
-
-            var distance = pointA.Distance(pointB);
-            /*END OF TESTING*/
-
+            var RealLocation = new Point(location.Y, location.X) { SRID = 4326 };
 
 
             // Fetch the tourist attractions and their
@@ -101,7 +44,7 @@ namespace VisitCalifornia.Controllers
             // using spatial queries.
             var touristAttractions = _dbContext
                 .TouristAttractions
-                .Select(t => new { Place = t, Distance = t.Location.Distance(location), RealDistance = 33.33 })
+                .Select(t => new { Place = t, Distance = t.Location.Distance(location), RealDistance = new Point(t.Location.Y, t.Location.X) { SRID = 4326 }.ProjectTo(2163).Distance(RealLocation.ProjectTo(2163)) })
                 .ToList();
 
             // Ordering the result in the ascending order of distance
--- VisitCalifornia/Extensions/GeometryExtensions.cs
+++ VisitCalifornia/Extensions/GeometryExtensions.cs
@@ -16,39 +16,25 @@ namespace VisitCalifornia.Extensions
                 new CoordinateTransformationFactory(),
                 new Dictionary<int, string>
                 {
-                // Coordinate systems:
+                    // Coordinate systems:
 
-                // (3857 and 4326 included automatically)
-                
+                    // (3857 and 4326 included automatically)
 
-                // This coordinate system covers the area of our data.
-                // Different data requires a different coordinate system.
-                [2855] =
-                    @"
-                    PROJCS[""NAD83(HARN) / Washington North"",
-                        GEOGCS[""NAD83(HARN)"",
-                            DATUM[""NAD83_High_Accuracy_Regional_Network"",
-                                SPHEROID[""GRS 1980"",6378137,298.257222101,
-                                    AUTHORITY[""EPSG"",""7019""]],
-                                AUTHORITY[""EPSG"",""6152""]],
-                            PRIMEM[""Greenwich"",0,
-                                AUTHORITY[""EPSG"",""8901""]],
-                            UNIT[""degree"",0.01745329251994328,
-                                AUTHORITY[""EPSG"",""9122""]],
-                            AUTHORITY[""EPSG"",""4152""]],
-                        PROJECTION[""Lambert_Conformal_Conic_2SP""],
-                        PARAMETER[""standard_parallel_1"",48.73333333333333],
-                        PARAMETER[""standard_parallel_2"",47.5],
-                        PARAMETER[""latitude_of_origin"",47],
-                        PARAMETER[""central_meridian"",-120.8333333333333],
-                        PARAMETER[""false_easting"",500000],
-                        PARAMETER[""false_northing"",0],
-                        UNIT[""metre"",1,
-                            AUTHORITY[""EPSG"",""9001""]],
-                        AUTHORITY[""EPSG"",""2855""]]
-                ",
-                [26911] =
-                    @"PROJCS[""NAD83 / UTM zone 11N"",
+
+                    // This coordinate system covers the area of our data.
+                    // Different data requires a different coordinate system.
+                    [4326] = @"GEOGCS[""WGS 84"",
+                        DATUM[""WGS_1984"",
+                            SPHEROID[""WGS 84"",6378137,298.257223563,
+                                AUTHORITY[""EPSG"",""7030""]],
+                            AUTHORITY[""EPSG"",""6326""]],
+                        PRIMEM[""Greenwich"",0,
+                            AUTHORITY[""EPSG"",""8901""]],
+                        UNIT[""degree"",0.0174532925199433,
+                            AUTHORITY[""EPSG"",""9122""]],
+                        AUTHORITY[""EPSG"",""4326""]]
+                    ",
+                    [2163] = @"PROJCS[""NAD83 / UTM zone 11N"",
                         GEOGCS[""NAD83"",
                             DATUM[""North_American_Datum_1983"",
                                 SPHEROID[""GRS 1980"",6378137,298.257222101,
@@ -71,8 +57,7 @@ namespace VisitCalifornia.Extensions
                         AXIS[""Easting"",EAST],
                         AXIS[""Northing"",NORTH],
                         AUTHORITY[""EPSG"",""26911""]]"
-
-                });
+        });
 
         public static Geometry ProjectTo(this Geometry geometry, int srid)
         {
